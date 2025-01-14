90326fcf677b3a0bf5ee1c526b19a57c19b94d93
satch <ems910@outlook.com>
2025-01-13 22:21:57 -0500

    update ddrtlsdr __init__

--- src/ddrtlsdr/__init__.py
+++ src/ddrtlsdr/__init__.py
@@ -1,5 +1,9 @@
 # src/ddrtlsdr/__init__.py
 
+from .logging_config import setup_logging
+
+setup_logging()
+
 from .device_manager import DeviceManager
 from .device_control import DeviceControl
 from .models import SDRDevice, SDRConfig
